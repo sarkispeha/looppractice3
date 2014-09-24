3b5fd658d2a01dd64d66eea92fc0c60449304a58
Sarkis Peha <sarkispeha@gmail.com>
2014-09-24 17:31:30 -0600

    finished

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
 $(document).on('ready', function() {
   
 });
+
+var students = [{
+ name: 'Liz',
+ age: 25,
+ city: 'Boulder'
+},{
+ name: 'Meghan',
+ age: 27,
+ city: 'Denver'
+},{
+ name: 'Trent',
+ age: 32,
+ city: 'Boulder'
+},{
+ name: 'Chelsea',
+ age: 24,
+ city: 'Boulder'
+},{
+ name: 'Amir',
+ age: 18,
+ city: 'Denver'
+}];
+
+for(var i = 0; i < students.length; i++) {
+	console.log(students[i].age);
+};
+
+for(var i = 0; i < students.length; i++) {
+	console.log(students[i].name + ", " + students[i].city);
+};
+
+for(var i = 0; i < students.length; i++) {
+	if(students[i].city === 'Boulder') {
+		console.log(students[i].name + ' is from ' + students[i].city);
+	}
+};
+
+for(var i = 0; i < students.length; i++) {
+	if(students[i].age > 25) {
+		console.log(students[i].name + " is older than 25");
+	}
+}
+
+
+
